bc3b40e46c00c9a807ccd6a5691acab1594ff5d2
will-parsons <wpparsons@gmail.com>
2015-10-20 10:40:03 +0100

    Create magento-apache.sh

---
+++ magento-apache.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+#
+# Script to set up Apache for Magento. 
+# Compiles mod_fastcgi for use with PHP-FPM. 
+#
+
+
+## Sanity checks - root on RHEL6 or CentOS 6
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit 1
+fi
+
+MAJORVERS=$(head -1 /etc/redhat-release | cut -d"." -f1 | egrep -o '[0-9]')
+if [ "$MAJORVERS"  != 6 ]; then
+   echo "This script is for CentOS 6 / RHEL 6  only."
+   exit 1
+fi
+echo "RHEL/CentOS 6 Confirmed."
+
