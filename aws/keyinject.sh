a47e30d4a6a3c0bf66e2340f63dd4f35087d8ebd
whyneus <minglong+github@gmail.com>
2016-02-24 13:52:49 +0000

    Create keyinject.sh

---
+++ aws/keyinject.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+keybucket=lsynckey-`/usr/local/bin/aws iam list-account-aliases | grep rax\\- | head -n1 | sed 's/"//g' | sed 's/[[:blank:]]//g'`
+bucketexist=`/usr/local/bin/aws s3 ls | grep -c ${keybucket}`
+if [ ${bucketexist} -eq 0 ];
+then
+  if [ ! -d ~magento/.ssh/ ];
+  then
+    mkdir ~magento/.ssh/
+    chown magento:magento ~magento/.ssh/
+  fi
+
+  if [ ! -f ~magento/.ssh/magento-admin ];
+  then
+    ssh-keygen -t rsa -C magento-admin -b 4096 -f ~magento/.ssh/magento-admin -q -N ""
+    chown magento:magento ~magento/.ssh/magento-admin*
+  fi
+  /usr/local/bin/aws s3 mb s3://${keybucket}/
+  /usr/local/bin/aws s3 cp "`getent passwd magento | cut -d: -f6`/.ssh/" s3://${keybucket}/ --recursive --include "magento-admin*"
+else
+  /usr/local/bin/aws s3 cp s3://${keybucket}/magento-admin `getent passwd magento | cut -d: -f6`/.ssh/
+fi
