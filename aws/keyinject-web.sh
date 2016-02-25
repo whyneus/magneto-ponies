2ae060cc7737e97e9a8da83b7c1a46925ddebbbe
whyneus <minglong+github@gmail.com>
2016-02-25 12:36:33 +0000

    Correct file referenced for ownership change

--- aws/keyinject-web.sh
+++ aws/keyinject-web.sh
@@ -14,7 +14,7 @@ if [ ! -z ${keybucket} ]
 then
   /usr/local/bin/aws s3 cp s3://${keybucket}/magento-admin.pub `getent passwd magento | cut -d: -f6`/.ssh/
   chmod 600 ~magento/.ssh/magento-admin
-  chown magento:magento ~magento/.ssh/magento-admin
+  chown magento:magento ~magento/.ssh/magento-admin.pub
 else
   exit 1
 fi
