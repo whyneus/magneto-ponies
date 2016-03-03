01797674ab4ce0c29a38b24e58775dce322ffbc6
whyneus <minglong+github@gmail.com>
2016-03-03 10:46:03 +0000

    Use files to store some stuff to reduce API calls

--- aws/lsync-elbpoll.sh
+++ aws/lsync-elbpoll.sh
@@ -3,9 +3,24 @@
 # The following IAM roles are required:
 #   AmazonEC2ReadOnlyAccess
 
-region=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/ | sed '$ s/.$//'`
 uuid=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
-rackuuid=`/usr/local/bin/aws ec2 describe-tags --region ${region} --filters "Name=resource-id,Values=${uuid}" "Name=key,Values=rackuuid" --query 'Tags[*].Value[]' --output text`
+
+if [ ! -f /tmp/awsregion ];
+then
+  echo `curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/ | sed '$ s/.$//'` > /tmp/awsregion
+  region=$(</tmp/awsregion)
+else
+  region=$(</tmp/awsregion)
+fi
+
+if [ ! -f /tmp/awstag ];
+then
+  echo `/usr/local/bin/aws ec2 describe-tags --region ${region} --filters "Name=resource-id,Values=${uuid}" "Name=key,Values=rackuuid" --query 'Tags[*].Value[]' --output text` > /tmp/awstag
+  rackuuid=$(</tmp/awstag)
+else
+  rackuuid=$(</tmp/awstag)
+fi
+
 elblist=`/usr/local/bin/aws elb describe-load-balancers --region ${region} --query 'LoadBalancerDescriptions[].LoadBalancerName[]' --output text`
 IFS=$'\t' read -ra elbcheck <<<"${elblist}"
 
@@ -16,7 +31,6 @@ do
   if [[ ${elbtag} == *"${rackuuid}"* ]]
   then
     ec2names=`/usr/local/bin/aws elb describe-instance-health --load-balancer-name ${i} --region ${region} --query 'InstanceStates[].InstanceId[]' --output text`
-echo $ec2names
 
     ec2addresses=`/usr/local/bin/aws ec2 describe-instances --region ${region} --filter Name=tag:rackuuid,Values=${rackuuid} --query 'Reservations[].Instances[].PrivateIpAddress[]' --instance-id ${ec2names} --output text`
   fi
