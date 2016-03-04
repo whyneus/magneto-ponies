33e1040795fcb18d34b9f4590fb2095b1d8bba2e
whyneus <minglong+github@gmail.com>
2016-03-04 07:22:07 +0000

    Output the IP list to a file for later use

--- aws/lsync-elbpoll.sh
+++ aws/lsync-elbpoll.sh
@@ -31,7 +31,9 @@ do
   if [[ ${elbtag} == *"${rackuuid}"* ]]
   then
     ec2names=`/usr/local/bin/aws elb describe-instance-health --load-balancer-name ${i} --region ${region} --query 'InstanceStates[].InstanceId[]' --output text`
-
     ec2addresses=`/usr/local/bin/aws ec2 describe-instances --region ${region} --filter Name=tag:rackuuid,Values=${rackuuid} --query 'Reservations[].Instances[].PrivateIpAddress[]' --instance-id ${ec2names} --output text`
+    break
   fi
 done
+
+echo ${ec2addresses} > /tmp/awsec2webips
