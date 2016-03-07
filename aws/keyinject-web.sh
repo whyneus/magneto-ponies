58dd85daac2873b4e3380c7d31f7581161c10882
whyneus <minglong+github@gmail.com>
2016-03-07 11:55:13 +0000

    Change aws executable path

--- aws/keyinject-web.sh
+++ aws/keyinject-web.sh
@@ -16,17 +16,17 @@ fi
 
 region=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/ | sed '$ s/.$//'`
 uuid=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
-keybucket=`/usr/local/bin/aws ec2 describe-tags --region ${region} --filters "Name=resource-id,Values=${uuid}" "Name=key,Values=rackuuid" --query 'Tags[*].Value[]' --output text`
+keybucket=`/bin/aws ec2 describe-tags --region ${region} --filters "Name=resource-id,Values=${uuid}" "Name=key,Values=rackuuid" --query 'Tags[*].Value[]' --output text`
 
 while [ -z ${bucketexist} ]
 do
-  bucketexist=`/usr/local/bin/aws s3 ls | grep "${keybucket}\-lsynckey"`
+  bucketexist=`/bin/aws s3 ls | grep "${keybucket}\-lsynckey"`
   sleep 5
 done
 
 if [ ! -z ${keybucket} ]
 then
-  /usr/local/bin/aws s3 cp s3://${keybucket}-lsynckey/magento-admin.pub `getent passwd magento | cut -d: -f6`/.ssh/
+  /bin/aws s3 cp s3://${keybucket}-lsynckey/magento-admin.pub `getent passwd magento | cut -d: -f6`/.ssh/
   chmod 600 ~magento/.ssh/magento-admin
   chown magento:magento ~magento/.ssh/magento-admin.pub
 fi
