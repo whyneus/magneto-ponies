e2983adba8ad03ea6a6efb6356f80f4871ad9a1d
whyneus <minglong+github@gmail.com>
2016-03-14 08:06:06 +0000

    Use a variable for home directory

--- aws/keyinject-admin.sh
+++ aws/keyinject-admin.sh
@@ -8,33 +8,34 @@
 #   AmazonS3FullAccess
 #   AmazonEC2ReadOnlyAccess
 
-if [ ! -d `getent passwd magento | cut -d: -f6`/.ssh/ ];
-then
-  mkdir `getent passwd magento | cut -d: -f6`/.ssh/
-  chown magento:magento `getent passwd magento | cut -d: -f6`/.ssh/
-fi
-
 region=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/ | sed '$ s/.$//'`
 uuid=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
 keybucket=`/bin/aws ec2 describe-tags --region ${region} --filters "Name=resource-id,Values=${uuid}" "Name=key,Values=rackuuid" --query 'Tags[*].Value[]' --output text`
 bucketexist=`/bin/aws s3 ls | grep -c "${keybucket}-lsynckey"`
+home=`getent passwd magento | cut -d: -f6`
+
+if [ ! -d ${home}/.ssh/ ];
+then
+  mkdir ${home}/.ssh/
+  chown magento:magento ${home}/.ssh/
+fi
 
 if [ ${bucketexist} != "1" ];
 then
-  if [ ! -f `getent passwd magento | cut -d: -f6`/.ssh/magento-admin ];
+  if [ ! -f ${home}/.ssh/magento-admin ];
   then
-    ssh-keygen -t rsa -C magento-admin -b 4096 -f `getent passwd magento | cut -d: -f6`/.ssh/magento-admin -q -N ""
-    chown magento:magento `getent passwd magento | cut -d: -f6`/.ssh/magento-admin*
-    cp -av `getent passwd magento | cut -d: -f6`/.ssh/magento-admin `getent passwd magento | cut -d: -f6`/.ssh/id_rsa
+    ssh-keygen -t rsa -C magento-admin -b 4096 -f ${home}/.ssh/magento-admin -q -N ""
+    chown magento:magento ${home}/.ssh/magento-admin*
+    cp -av ${home}/.ssh/magento-admin ${home}/.ssh/id_rsa
   fi
   /bin/aws s3 mb s3://${keybucket}-lsynckey/
   /bin/aws s3api put-bucket-tagging --bucket ${keybucket}-lsynckey --tagging "TagSet=[{Key=rackuuid,Value=${keybucket}}]"
-  /bin/aws s3 cp --sse AES256 "`getent passwd magento | cut -d: -f6`/.ssh/" s3://${keybucket}-lsynckey/ --recursive --include "magento-admin*"
+  /bin/aws s3 cp --sse AES256 "${home}/.ssh/" s3://${keybucket}-lsynckey/ --recursive --include "magento-admin*"
 else
-  /bin/aws s3 cp s3://${keybucket}-lsynckey/magento-admin `getent passwd magento | cut -d: -f6`/.ssh/
-  chmod 600 `getent passwd magento | cut -d: -f6`/.ssh/magento-admin
-  chown magento:magento `getent passwd magento | cut -d: -f6`/.ssh/magento-admin
-  cp -av `getent passwd magento | cut -d: -f6`/.ssh/magento-admin `getent passwd magento | cut -d: -f6`/.ssh/id_rsa
+  /bin/aws s3 cp s3://${keybucket}-lsynckey/magento-admin ${home}/.ssh/
+  chmod 600 ${home}/.ssh/magento-admin
+  chown magento:magento ${home}/.ssh/magento-admin
+  cp -av ${home}/.ssh/magento-admin ${home}/.ssh/id_rsa
 fi
 
 if [ ! -f /opt/rackspace/lsyncd-elbpoll.sh ]
