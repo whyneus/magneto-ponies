add5d259ad89a9680a8a0b6dc139914b2e638af7
whyneus <minglong+github@gmail.com>
2016-03-15 13:06:23 +0000

    Update media-setup.sh

--- aws/media-setup.sh
+++ aws/media-setup.sh
@@ -8,7 +8,7 @@ user="magento"
 region=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/ | sed '$ s/.$//'`
 uuid=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
 mediabucket="`/bin/aws ec2 describe-tags --region ${region} --filters "Name=resource-id,Values=${uuid}" "Name=key,Values=rackuuid" --query 'Tags[*].Value[]' --output text`"
-bucketexist=`/bin/aws s3 ls | grep -c "${mediabucket}-media"`
+bucketexist=`/bin/aws s3 ls --region ${region} | grep -c "${mediabucket}-media"`
 home=`getent passwd ${user} | cut -d: -f6`
 
 if [ ! -d ${home}/httpdocs/media ]
@@ -21,7 +21,7 @@ fi
 if [ ${bucketexist} != "1" ];
 then
   /bin/aws s3 mb s3://${mediabucket}-media/ --region ${region}
-  /bin/aws s3api put-bucket-tagging --bucket ${mediabucket}-media --tagging "TagSet=[{Key=rackuuid,Value=${mediabucket}}]"
+  /bin/aws s3api put-bucket-tagging --region ${region} --bucket ${mediabucket}-media --tagging "TagSet=[{Key=rackuuid,Value=${mediabucket}}]" 
 else
   /bin/aws s3 sync s3://${mediabucket}-media/ ${home}/httpdocs/media/ --delete --quiet --region ${region}
   chown -R ${user}:${user} ${home}/httpdocs/media
