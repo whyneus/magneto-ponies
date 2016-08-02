4b53f5d39e3b31e29371a85bf987a14af9086eba
will-parsons <wpparsons@gmail.com>
2016-08-02 14:53:30 +0100

    Create magento-logrorate.sh

---
+++ magento-logrorate.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+#  Simple logrotate config for Magento servers. 
+
+echo "
+/var/www/vhosts/*/httpdocs/var/log/system.log /var/www/vhosts/*/httpdocs/var/log/exception.log  {
+    weekly
+    rotate 8
+    copytruncate
+    delaycompress
+    notifempty
+    missingok
+}
+
+# In case Magento is using /tmp as a fallback option
+/tmp/magento/var/log/*.log  {
+    daily
+    rotate 7
+    copytruncate
+    notifempty
+    compress
+    missingok
+}
+" >> /etc/lodrotate.d/magento
